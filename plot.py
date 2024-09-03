38ac5e9878e6875952ff0481fe729ad3e1f12d4d
Zach Miller <zmiller@ucsb.edu>
2024-09-03 14:39:10 -0700

    adds plotting for garbage collector w-amp

--- plot.py
+++ plot.py
@@ -1,39 +1,99 @@
 import matplotlib.pyplot as plt
 import sys
 
-def read_numbers(file_path, max_size_gb):
-    numbers = []
+def read_wamp(file_path, max_size_gb):
+    wamp, gb_wrtn = [], []
     with open(file_path, 'r') as file:
       for line in file:
         line_split = line.split()
         if line_split[1] == 'GB' and float(line_split[0]) > max_size_gb:
            break
-        numbers.append(float(line_split[2]))
-    return numbers
+        wamp.append(float(line_split[3]))
+        gb_wrtn.append(float(line_split[2]))
+    return wamp, gb_wrtn
 
-def plot_numbers(rocks, autumn_6):
-    indices_rocks = range(1, len(rocks) + 1)
-    indices_6 = range(1, len(autumn_6) + 1)
+def read_iostat(file_path, num_lines):
+    gb_total_wrtn = []
+    with open(file_path, 'r') as file:
+        line_no = 0
+        baseline = None
+        for line in file:
+            if line_no == num_lines + 1:
+                break
+            kb_wrtn = float(line.strip())
+            if baseline is None:
+                baseline = kb_wrtn
+            else:
+                gb_total_wrtn.append((kb_wrtn - baseline) / pow(10, 6))
+            line_no += 1
+    return gb_total_wrtn
+
+def read_levels(file_path, num_lines):
+    levels = []
+    with open(file_path, 'r') as file:
+        line_no = 0
+        for line in file:
+            if line_no == num_lines:
+                break
+            levels.append(float(line.strip()))
+            line_no += 1
+    return levels
+
+def plot(wamp_rocks, levels_rocks, gc_wamp_rocks, wamp_6, levels_6, gc_wamp_6, wamp_8, levels_8, gc_wamp_8):
+    indices_rocks = range(1, len(wamp_rocks) + 1)
+    indices_6 = range(1, len(wamp_6) + 1)
+    indices_8 = range(1, len(wamp_8) + 1)
     
-    # Plot both datasets
-    plt.plot(indices_rocks, rocks, linestyle='-', color='b', label="RocksDB")
-    plt.plot(indices_6, autumn_6, linestyle='-', color='r', label="Autumn 0.6")
+    _, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 8))
     
-    plt.title("Write Amplification Comparison")
-    plt.xlabel("Time (min)")
-    plt.ylabel("Sum W-amp")
-    plt.grid(True)
+    ax1.plot(indices_rocks, wamp_rocks, linestyle='-', color='b', label="RocksDB")
+    ax1.plot(indices_6, wamp_6, linestyle='-', color='r', label="Autumn 0.6")
+    ax1.plot(indices_8, wamp_8, linestyle='-', color='g', label="Autumn 0.8")
     
-    # Add legend
-    plt.legend()
-    
+    ax1.set_title("Write Amplification")
+    ax1.set_xlabel("Time (min)")
+    ax1.set_ylabel("Sum W-amp")
+    ax1.grid(True)
+    ax1.legend()
+
+    ax2.plot(indices_rocks, levels_rocks, linestyle='-', color='b', label="RocksDB")
+    ax2.plot(indices_6, levels_6, linestyle='-', color='r', label="Autumn 0.6")
+    ax2.plot(indices_8, levels_8, linestyle='-', color='g', label="Autumn 0.8")
+
+    ax2.set_title("Levels")
+    ax2.set_xlabel("Time (min)")
+    ax2.set_ylabel("Number of Levels")
+    ax2.grid(True)
+    ax2.legend()
+
+    ax3.plot(indices_rocks, gc_wamp_rocks, linestyle='-', color='b', label="RocksDB")
+    ax3.plot(indices_6, gc_wamp_6, linestyle='-', color='r', label="Autumn 0.6")
+    ax3.plot(indices_8, gc_wamp_8, linestyle='-', color='g', label="Autumn 0.8")
+
+    ax3.set_title("GC Write Amplification")
+    ax3.set_xlabel("Time (min)")
+    ax3.set_ylabel("GC W-amp")
+    ax3.set_ylim(bottom=1.0)
+    ax3.grid(True)
+    ax3.legend()
+
+    plt.tight_layout()
     plt.show()
 
 if __name__ == "__main__":
-    path_rocks = sys.argv[1]  
-    rocks = read_numbers(path_rocks, 200)
+    wamp_rocks, gb_wrtn_rocks = read_wamp(sys.argv[1], 200)
+    levels_rocks = read_levels(sys.argv[2], len(wamp_rocks))
+    writes_rocks = read_iostat(sys.argv[3], len(wamp_rocks))
+    gc_wamp_rocks = [writes_rocks[i] / gb_wrtn_rocks[i] for i in range(len(wamp_rocks))]
 
-    path_autumn = sys.argv[2]  
-    autumn_6 = read_numbers(path_autumn, 200)
+    wamp_6, gb_wrtn_6 = read_wamp(sys.argv[4], 200)
+    levels_6 = read_levels(sys.argv[5], len(wamp_6))
+    writes_6 = read_iostat(sys.argv[6], len(wamp_6))
+    gc_wamp_6 = [writes_6[i] / gb_wrtn_6[i] for i in range(len(wamp_6))]
+
+    wamp_8, gb_wrtn_8 = read_wamp(sys.argv[7], 200)
+    levels_8 = read_levels(sys.argv[8], len(wamp_8))
+    writes_8 = read_iostat(sys.argv[9], len(wamp_8))
+    gc_wamp_8 = [writes_8[i] / gb_wrtn_8[i] for i in range(len(wamp_8))]
     
-    plot_numbers(rocks, autumn_6)
+    plot(wamp_rocks, levels_rocks, gc_wamp_rocks, wamp_6, levels_6, gc_wamp_6, wamp_8, levels_8, gc_wamp_8)
--- prune_and_plot.sh
+++ prune_and_plot.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 LOG_ROCKS LOG_0.6"
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 LOG_ROCKSDB LOG_0.6 LOG_0.8"
     exit 1
 fi
 
-grep "Sum" $1 | tr -s ' ' | cut -d ' ' -f4 -f5 -f13 > output1.txt
-grep "Sum" $2 | tr -s ' ' | cut -d ' ' -f4 -f5 -f13 > output2.txt
+grep "Sum" $1 | tr -s ' ' | cut -d ' ' -f4 -f5 -f10 -f13 > wamp_$1.txt
+grep "Sum" $2 | tr -s ' ' | cut -d ' ' -f4 -f5 -f10 -f13 > wamp_$2.txt
+grep "Sum" $3 | tr -s ' ' | cut -d ' ' -f4 -f5 -f10 -f13 > wamp_$3.txt
 
-python plot.py output1.txt output2.txt
+sed -n '/Sum/{x;1!p;}; h' $1| sed 's/^[^0-9]*\([0-9]*\).*/\1/' > levels_$1.txt
+sed -n '/Sum/{x;1!p;}; h' $2| sed 's/^[^0-9]*\([0-9]*\).*/\1/' > levels_$2.txt
+sed -n '/Sum/{x;1!p;}; h' $3| sed 's/^[^0-9]*\([0-9]*\).*/\1/' > levels_$3.txt
 
-rm output1.txt output2.txt
+grep "nvme1n1" $1_iostat | tr -s ' ' | cut -d ' ' -f7 > writes_$1.txt
+grep "nvme1n1" $2_iostat | tr -s ' ' | cut -d ' ' -f7 > writes_$2.txt
+grep "nvme1n1" $3_iostat | tr -s ' ' | cut -d ' ' -f7 > writes_$3.txt
+
+python plot.py wamp_$1.txt levels_$1.txt writes_$1.txt wamp_$2.txt levels_$2.txt writes_$2.txt wamp_$3.txt levels_$3.txt writes_$3.txt
+
+rm wamp_$1.txt levels_$1.txt writes_$1.txt wamp_$2.txt levels_$2.txt writes_$2.txt wamp_$3.txt levels_$3.txt writes_$3.txt
